55c670dc4608d3a0357220a51c76e16f7a9956a2
Tim Kadlec <tim@timkadlec.com>
2015-05-20 15:07:15 -0500

    massive update

---
+++ transitions/page2.html.php
@@ -0,0 +1,21 @@
+<?php
+    header('Link: <test.css>;rel=transition-entering-stylesheet;scope=*');
+?>
+<!doctype html>
+<html>
+<head>
+  <!-- Transition the grumpy_cat thumbnail image to any origin -->
+  <meta name="viewport" content="width=device-width" />
+  <meta name="transition-elements" content="#val-cat,#msg;*">
+  <link rel="stylesheet" href="blah.css">  <!-- Define an exit animation for the page -->
+</head>
+<body>
+    <div id="msg" class="cat-msg">
+  
+  <div class="text">
+  <h4>NEW CAT CHAT MESSAGE:</h4>
+  <p>Cyril would like to discuss the current status of his water dish.</p>
+  </div>
+   <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/13034/cyril.jpg" id="val-cat" class="kitten" alt="Cyril"  ></div>
+</body>
+</html>
---
+++ transitions/starry_night/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<!-- saved from url=(0049)http://navtransoysteine.appspot.com/starry_night/ -->
+<html style=""><head xmlns="http://www.w3.org/1999/html"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7">
+    <meta http-equiv="Cache-Control" content="no-store">
+    
+
+    <meta name="transition-elements" content="#hplogo,#searchform,#searchbuttonDiv;*">
+
+    <style type="text/css">.gm-style .gm-style-cc span,.gm-style .gm-style-cc a,.gm-style .gm-style-mtc div{font-size:10px}</style>
+    <style type="text/css">@media print {  .gm-style .gmnoprint, .gmnoprint {    display:none  }}@media screen {  .gm-style .gmnoscreen, .gmnoscreen {    display:none  }}</style>
+    <style type="text/css">.gm-style{font-family:Roboto,Arial,sans-serif;font-size:11px;font-weight:400;text-decoration:none}</style>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, user-scalable=0">
+    <meta name="google" value="notranslate">
+
+    <title>The Starry Night - Google Search</title>
+
+    <link rel="stylesheet" type="text/css" href="./assets/style.css">
+    <link rel="stylesheet" type="text/css" href="./assets/header.css">
+
+    <link rel="transition-exiting-stylesheet" type="text/css" href="./assets/exit-transition.css">
+
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="QP">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+<style id="style-1-cropbar-clipper">/* Copyright 2014 Evernote Corporation. All rights reserved. */
+.en-markup-crop-options {
+    top: 18px !important;
+    left: 50% !important;
+    margin-left: -100px !important;
+    width: 200px !important;
+    border: 2px rgba(255,255,255,.38) solid !important;
+    border-radius: 4px !important;
+}
+
+.en-markup-crop-options div div:first-of-type {
+    margin-left: 0px !important;
+}
+</style></head>
+<body class="">
+
+<div id="block" style="display: none;"></div>
+
+<div id="root" style="visibility: visible;">
+
+    <div id="container">
+        <div id="search-form" class="">
+            <h1><img height="64" src="./assets/mlogo2x_3.png" width="183" id="hplogo"></h1>
+            <form onsubmit="return false;" autocomplete="off">
+                <div id="searchform" class="rich-search-form">
+                    <input name="q" value="The Starry Night">
+                </div>
+                <div id="searchbuttonDiv">
+                <button id="searchbutton" type="submit" class="search-button">
+                    <span class="search-icon"></span>
+                </button>
+                </div>
+                <p style="display: none;"></p>
+            </form>
+        </div>
+
+
+    </div>
+</div>
+
+<!-- <img id='grumpy' src="./index_files/grumpy.jpg" style="position:absolute; left:100px; top:400px; " display: none/> -->
+
+<script>
+    document.getElementById('searchbutton').onclick = function() {
+        window.location.href = 'results.php';
+    };
+</script>
+
+<div style="width: 100%; height: 100%; position: fixed; left: 0px; top: 0px; z-index: 1; pointer-events: none; visibility: hidden;"></div>
+
+
+
+<div id="window-resizer-tooltip"><a href="http://navtransoysteine.appspot.com/starry_night/#" title="Edit settings" style="background-image: url(chrome-extension://kkelicaakdanhinjdeammmilcgefonfh/images/icon_19.png);"></a><span class="tooltipTitle">Window size: </span><span class="tooltipWidth" id="winWidth"></span> x <span class="tooltipHeight" id="winHeight"></span><br><span class="tooltipTitle">Viewport size: </span><span class="tooltipWidth" id="vpWidth"></span> x <span class="tooltipHeight" id="vpHeight"></span></div></body></html>
